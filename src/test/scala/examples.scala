dd9247688e7443112a35f8a4fda9ac5117b5a6cb
Joni Freeman <joni.freeman@reaktor.fi>
2012-08-10 15:43:44 +0300

    Simplify a bit

--- src/test/scala/examples.scala
+++ src/test/scala/examples.scala
@@ -5,25 +5,20 @@ import org.scalatest._
 import Sql._ // FIXME move to package object
 
 class ExampleSuite extends FunSuite with matchers.ShouldMatchers {
+  Class.forName("com.mysql.jdbc.Driver")
+
   object Columns {
     object name
     object age
   }
 
   implicit val c = Configuration("jdbc:mysql://localhost:3306/sqltyped", "com.mysql.jdbc.Driver", "root", "", Columns)
+  val conn = DriverManager.getConnection(c.url, c.username, c.password)
 
   import Columns._
 
   test("Simple query") {
-    val conn = Connection.createConnection(c.url, c.username, c.password)
     val q = sql("select name, age from person")
     query(conn, q).map(p => p.get(age)).sum should equal (50)
   }
-}
-
-object Connection {
-  Class.forName("com.mysql.jdbc.Driver")
-
-  def createConnection(url: String, username: String, password: String) = 
-    DriverManager.getConnection(url, username, password)
 }
