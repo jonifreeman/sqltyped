ba74c121e5560c011e8da81413213f0588193860
Joni Freeman <joni.freeman@reaktor.fi>
2012-09-23 18:53:53 +0300

    Use lazy vals for parser defs

--- src/main/scala/parser.scala
+++ src/main/scala/parser.scala
@@ -11,61 +11,61 @@ object SqlParser extends RegexParsers {
     case err: NoSuccess => Left(err.msg)
   }
 
-  def stmt = (unionStmt | selectStmt | insertStmt | updateStmt | deleteStmt | createStmt)
+  lazy val stmt = (unionStmt | selectStmt | insertStmt | updateStmt | deleteStmt | createStmt)
 
-  def selectStmt = select ~ from ~ opt(where) ~ opt(groupBy) ~ opt(orderBy) ~ opt(limit) ^^ {
+  lazy val selectStmt = select ~ from ~ opt(where) ~ opt(groupBy) ~ opt(orderBy) ~ opt(limit) ^^ {
     case s ~ f ~ w ~ g ~ o ~ l => Select(s, f, w, g, o, l)
   }
 
-  def unionStmt = optParens(selectStmt) ~ "union".i ~ optParens(selectStmt) ~ opt(orderBy) ~ opt(limit) ^^ {
+  lazy val unionStmt = optParens(selectStmt) ~ "union".i ~ optParens(selectStmt) ~ opt(orderBy) ~ opt(limit) ^^ {
     case s1 ~ _ ~ s2 ~ o ~ l => Union(s1, s2, o, l)
   }
 
-  def insertStmt = "insert".i ~ "into".i ~ table ~ opt(colNames) ~ (listValues | selectValues) ^^ {
+  lazy val insertStmt = "insert".i ~ "into".i ~ table ~ opt(colNames) ~ (listValues | selectValues) ^^ {
     case _ ~ _ ~ t ~ cols ~ vals => Insert(t, cols, vals)
   }
 
-  def colNames = "(" ~> repsep(ident, ",") <~ ")"
+  lazy val colNames = "(" ~> repsep(ident, ",") <~ ")"
 
-  def listValues = "values".i ~> "(" ~> repsep(term, ",") <~ ")" ^^ ListedInput.apply
+  lazy val listValues = "values".i ~> "(" ~> repsep(term, ",") <~ ")" ^^ ListedInput.apply
 
-  def selectValues = selectStmt ^^ SelectedInput.apply
+  lazy val selectValues = selectStmt ^^ SelectedInput.apply
 
-  def updateStmt = "update".i ~ repsep(table, ",") ~ "set".i ~ repsep(assignment, ",") ~ opt(where) ~ opt(orderBy) ~ opt(limit) ^^ {
+  lazy val updateStmt = "update".i ~ repsep(table, ",") ~ "set".i ~ repsep(assignment, ",") ~ opt(where) ~ opt(orderBy) ~ opt(limit) ^^ {
     case _ ~ t ~ _ ~ a ~ w ~ o ~ l => Update(t, a, w, o, l)
   }
 
-  def assignment = column ~ "=" ~ term ^^ { case c ~ _ ~ t => (c, t) }
+  lazy val assignment = column ~ "=" ~ term ^^ { case c ~ _ ~ t => (c, t) }
 
-  def deleteStmt = "delete".i ~ opt(repsep(ident, ",")) ~ from ~ opt(where) ^^ {
+  lazy val deleteStmt = "delete".i ~ opt(repsep(ident, ",")) ~ from ~ opt(where) ^^ {
     case _ ~ f ~ w => Delete(f, w)
   }
 
-  def createStmt = "create".i ^^^ Create
+  lazy val createStmt = "create".i ^^^ Create
 
-  def select: Parser[List[Value]] = "select".i ~> repsep((opt("distinct".i) ~> value), ",")
+  lazy val select: Parser[List[Value]] = "select".i ~> repsep((opt("distinct".i) ~> value), ",")
 
-  def from: Parser[List[From]] = "from".i ~> rep1sep(join, ",")
+  lazy val from: Parser[List[From]] = "from".i ~> rep1sep(join, ",")
 
-  def join = table ~ rep(joinSpec) ^^ { case t ~ j => From(t, j) }
+  lazy val join = table ~ rep(joinSpec) ^^ { case t ~ j => From(t, j) }
 
-  def joinSpec = opt("left".i | "right".i) ~ opt("inner".i | "outer".i) ~ "join".i ~ table ~ "on".i ~ expr ^^ {
+  lazy val joinSpec = opt("left".i | "right".i) ~ opt("inner".i | "outer".i) ~ "join".i ~ table ~ "on".i ~ expr ^^ {
     case side ~ joinType ~ _ ~ table ~ "on" ~ expr => 
       Join(table, expr, side.map(_ + " ").getOrElse("") + joinType.map(_ + " ").getOrElse("") + "join")
   }
 
-  def table = ident ~ opt(opt("as".i) ~> ident) ^^ { case n ~ a => Table(n, a) }
+  lazy val table = ident ~ opt(opt("as".i) ~> ident) ^^ { case n ~ a => Table(n, a) }
 
-  def where = "where".i ~> expr ^^ Where.apply
+  lazy val where = "where".i ~> expr ^^ Where.apply
 
-  def expr: Parser[Expr] = (predicate | parens)* (
-      "and" ^^^ { (e1: Expr, e2: Expr) => And(e1, e2) } 
-    | "or"  ^^^ { (e1: Expr, e2: Expr) => Or(e1, e2) } 
+  lazy val expr: Parser[Expr] = (predicate | parens)* (
+      "and".i ^^^ { (e1: Expr, e2: Expr) => And(e1, e2) } 
+    | "or".i  ^^^ { (e1: Expr, e2: Expr) => Or(e1, e2) } 
   )
 
-  def parens: Parser[Expr] = "(" ~> expr  <~ ")"
+  lazy val parens: Parser[Expr] = "(" ~> expr  <~ ")"
 
-  def predicate: Parser[Predicate] = (
+  lazy val predicate: Parser[Predicate] = (
       term ~ "="  ~ (term | subselect)       ^^ { case lhs ~ _ ~ rhs => Predicate2(lhs, Eq, rhs) }
     | term ~ "!=" ~ (term | subselect)       ^^ { case lhs ~ _ ~ rhs => Predicate2(lhs, Neq, rhs) }
     | term ~ "<"  ~ (term | subselect)       ^^ { case lhs ~ _ ~ rhs => Predicate2(lhs, Lt, rhs) }
@@ -78,11 +78,11 @@ object SqlParser extends RegexParsers {
     | term <~ "is".i ~ "not".i ~ "null".i    ^^ { t => Predicate1(t, IsNotNull) }
   )
 
-  def subselect = "(" ~> selectStmt <~ ")" ^^ Subselect.apply
+  lazy val subselect = "(" ~> selectStmt <~ ")" ^^ Subselect.apply
 
-  def term = (arith | simpleTerm)
+  lazy val term = (arith | simpleTerm)
 
-  def simpleTerm: Parser[Term] = (
+  lazy val simpleTerm: Parser[Term] = (
       boolean    ^^ (b => Constant(typeOf[Boolean], b))
     | stringLit  ^^ (s => Constant(typeOf[String], s))
     | numericLit ^^ (n => if (n.contains(".")) Constant(typeOf[Double], n.toDouble) else Constant(typeOf[Long], n.toInt))
@@ -91,9 +91,9 @@ object SqlParser extends RegexParsers {
     | "?"        ^^^ Input
   )
 
-  def value = (arith | simpleValue)
+  lazy val value = (arith | simpleValue)
 
-  def simpleValue: Parser[Value] = (
+  lazy val simpleValue: Parser[Value] = (
       boolean    ^^ (b => Constant(typeOf[Boolean], b))
     | stringLit  ^^ (s => Constant(typeOf[String], s))
     | numericLit ^^ (n => if (n.contains(".")) Constant(typeOf[Double], n.toDouble) else Constant(typeOf[Long], n.toInt))
@@ -102,21 +102,21 @@ object SqlParser extends RegexParsers {
     | allColumns
   )
 
-  def column = (
+  lazy val column = (
       ident ~ "." ~ ident ~ "as".i ~ ident ^^ { case t ~ _ ~ c ~ _ ~ a => Column(c, Some(t), Some(a)) }
     | ident ~ "." ~ ident ^^ { case t ~ _ ~ c => Column(c, Some(t), None) }
     | ident ~ "as".i ~ ident ^^ { case c ~ _ ~ a => Column(c, None, Some(a)) }
     | ident ^^ (c => Column(c, None, None))
   )
 
-  def allColumns = "*" ~ opt("." ~> ident) ^^ { case _ ~ t => AllColumns(t) }
+  lazy val allColumns = "*" ~ opt("." ~> ident) ^^ { case _ ~ t => AllColumns(t) }
 
-  def function: Parser[Function] = 
+  lazy val function: Parser[Function] = 
     ident ~ "(" ~ repsep(term, ",") ~ ")" ~ opt("as".i ~> ident) ^^ {
       case name ~ _ ~ params ~ _ ~ alias => Function(name, params, alias)
     }
 
-  def arith: Parser[Value] = (simpleValue | arithParens)* (
+  lazy val arith: Parser[Value] = (simpleValue | arithParens)* (
       "+" ^^^ { (lhs: Value, rhs: Value) => ArithExpr(lhs, "+", rhs) }
     | "-" ^^^ { (lhs: Value, rhs: Value) => ArithExpr(lhs, "-", rhs) }
     | "*" ^^^ { (lhs: Value, rhs: Value) => ArithExpr(lhs, "*", rhs) }
@@ -124,25 +124,25 @@ object SqlParser extends RegexParsers {
     | "%" ^^^ { (lhs: Value, rhs: Value) => ArithExpr(lhs, "%", rhs) }
   )
 
-  def arithParens = "(" ~> arith <~ ")"
+  lazy val arithParens = "(" ~> arith <~ ")"
 
-  def boolean = ("true".i ^^^ true | "false".i ^^^ false)
+  lazy val boolean = ("true".i ^^^ true | "false".i ^^^ false)
 
-  def orderBy = "order".i ~> "by".i ~> repsep(column, ",") ~ opt("asc".i ^^^ Asc | "desc".i ^^^ Desc) ^^ {
+  lazy val orderBy = "order".i ~> "by".i ~> repsep(column, ",") ~ opt("asc".i ^^^ Asc | "desc".i ^^^ Desc) ^^ {
     case cols ~ order => OrderBy(cols, order)
   }
 
-  def groupBy = "group".i ~> "by".i ~> column ~ opt(having) ^^ {
+  lazy val groupBy = "group".i ~> "by".i ~> column ~ opt(having) ^^ {
     case col ~ having => GroupBy(col, having)
   }
 
-  def having = "having".i ~> expr ^^ Having.apply
+  lazy val having = "having".i ~> expr ^^ Having.apply
 
-  def limit = "limit".i ~> intOrInput ~ opt("offset".i ~> intOrInput) ^^ {
+  lazy val limit = "limit".i ~> intOrInput ~ opt("offset".i ~> intOrInput) ^^ {
     case count ~ offset => Limit(count, offset)
   }
 
-  def intOrInput = (
+  lazy val intOrInput = (
       "?" ^^^ Right(Input)
     | numericLit ^^ (n => Left(n.toInt))
   )
