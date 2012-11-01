93f70770563291286732329449b5b4ee3a078fae
Joni Freeman <joni.freeman@reaktor.fi>
2012-11-01 22:20:42 +0200

    Improve JSON example

--- core/src/test/scala/recordexamples.scala
+++ core/src/test/scala/recordexamples.scala
@@ -6,25 +6,32 @@ import shapeless._
 
 object JSON {
   object fieldToJSON extends Poly1 {
-    implicit def longToJSON[K] = at[(K, Long)] { 
+    implicit def numToJSON[K, V <% Number] = at[(K, V)] { 
       case (k, v) => "\"" + keyAsString(k) + "\":" + v.toString 
     }
-    implicit def intToJSON[K] = at[(K, Int)] { 
-      case (k, v) => "\"" + keyAsString(k) + "\":" + v.toString 
-    }
-    implicit def stringToJSON[K] = at[(K, String)] { 
+    implicit def stringToJSON[K, V <% String] = at[(K, V)] { 
       case (k, v) => "\"" + keyAsString(k) + "\":\"" + v + "\""
     }
-    implicit def pkToJSON[K, T] = at[(K, Long @@ T)] { 
+    implicit def boolToJSON[K, V <% Boolean] = at[(K, V)] { 
       case (k, v) => "\"" + keyAsString(k) + "\":" + v.toString 
-    }    
+    }
+    implicit def tstampToJSON[K, V <% java.sql.Timestamp] = at[(K, V)] { 
+      case (k, v) => "\"" + keyAsString(k) + "\":\"" + v.toString + "\""
+    }
+    implicit def optionToJSON[K, V](implicit c: Case1[(K, V)]) = at[(K, Option[V])] { 
+      case (k, Some(v)) => fieldToJSON((k, v)).toString
+      case (k, None) => ""
+    }
   }
 
-  def toJSON[R <: HList](rows: List[R])(implicit foldMap: MapFolder[R, String, fieldToJSON.type]) = {
+  def toJSON[R <: HList](record: R)(implicit foldMap: MapFolder[R, String, fieldToJSON.type]): String = {
     val concat = (s1: String, s2: String) => if (s2 != "") s1 + "," + s2 else s1
 
-    (rows map (row => "{" + (row.foldMap("")(fieldToJSON)(concat)) + "}")).mkString("[", ",", "]")
+    "{" + (record.foldMap("")(fieldToJSON)(concat)) + "}"
   }
+
+  def toJSON[R <: HList](records: List[R])(implicit foldMap: MapFolder[R, String, fieldToJSON.type]): String = 
+    (records map (r => toJSON(r))).mkString("[", ",", "]")
 }
 
 class RecordExampleSuite extends Example {
