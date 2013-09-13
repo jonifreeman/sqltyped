fdb89b8fcec2fbfaebae791e428dc3cc0f79a13c
Joni Freeman <joni.freeman@reaktor.fi>
2013-09-13 09:34:03 +0300

    Force result to be JValue. Improves compilation time a lot.

--- json4s/src/main/scala/json.scala
+++ json4s/src/main/scala/json.scala
@@ -23,47 +23,47 @@ object Record {
 }
 
 object JSON {
-  def compact[A](a: A)(implicit ctoj: toJSON.Case[A] { type Result <: JValue }): String = 
+  def compact[A](a: A)(implicit ctoj: toJSON.Case[A] { type Result = JValue }): String = 
     JsonMethods.compact(render(ctoj(a)))
 
-  def pretty[A](a: A)(implicit ptoj: toJSON.Case[A] { type Result <: JValue }): String = 
+  def pretty[A](a: A)(implicit ptoj: toJSON.Case[A] { type Result = JValue }): String = 
     JsonMethods.pretty(render(ptoj(a)))
 }
 
 object toJSON extends Poly1 {
-  implicit def atNull = at[Null](_ => JNull)
-  implicit def atDouble = at[Double](JDouble(_))
-  implicit def atBigInt = at[BigInt](JInt(_))
-  implicit def atBigDecimal = at[BigDecimal](JDecimal(_))
-  implicit def atNumber[V <% Long] = at[V](i => JInt(BigInt(i)))
-  implicit def atString = at[String](s => if (s == null) JNull else JString(s))
-  implicit def atBoolean = at[Boolean](JBool(_))
-  implicit def atJSON[V <: JValue] = at[V](identity)
+  implicit def atNull = at[Null](_ => JNull : JValue)
+  implicit def atDouble = at[Double](JDouble(_) : JValue)
+  implicit def atBigInt = at[BigInt](JInt(_) : JValue)
+  implicit def atBigDecimal = at[BigDecimal](JDecimal(_) : JValue)
+  implicit def atNumber[V <% Long] = at[V](i => JInt(BigInt(i)) : JValue)
+  implicit def atString = at[String](s => (if (s == null) JNull else JString(s)) : JValue)
+  implicit def atBoolean = at[Boolean](JBool(_) : JValue)
+  implicit def atJSON[V <: JValue] = at[V](json => json : JValue)
 
-  implicit def atTagged[V, T](implicit ttoj: toJSON.Case[V] { type Result <: JValue }) =
-    at[V @@ T](v => ttoj(v: V))
+  implicit def atTagged[V, T](implicit ttoj: toJSON.Case[V] { type Result = JValue }) =
+    at[V @@ T](v => ttoj(v: V) : JValue)
 
-  implicit def atOption[V](implicit otoj: toJSON.Case[V] { type Result <: JValue }) =
+  implicit def atOption[V](implicit otoj: toJSON.Case[V] { type Result = JValue }) =
     at[Option[V]] {
       case Some(v) => otoj(v : V) : JValue
-      case None    => JNull
+      case None    => JNull : JValue
     }
   
   implicit def atDate[V <: java.util.Date](implicit f: Formats) =
-    at[V](s => if (s == null) JNull else JString(f.dateFormat.format(s)))
+    at[V](s => (if (s == null) JNull else JString(f.dateFormat.format(s))) : JValue)
 
-  implicit def atTraversable[V, C[V] <: Traversable[V]](implicit ttoj: toJSON.Case[V] { type Result <: JValue }) =
-    at[C[V]](l => JArray(l.toList.map(v => ttoj(v : V) : JValue)))
+  implicit def atTraversable[V, C[V] <: Traversable[V]](implicit ttoj: toJSON.Case[V] { type Result = JValue }) =
+    at[C[V]](l => JArray(l.toList.map(v => ttoj(v : V) : JValue)) : JValue)
 
-  implicit def atMap[K, V](implicit mtoj: toJSON.Case[V] { type Result <: JValue }) =
-    at[Map[K, V]](m => JObject(m.toList.map(e => JField(e._1.toString, mtoj(e._2 : V) : JValue))))
+  implicit def atMap[K, V](implicit mtoj: toJSON.Case[V] { type Result = JValue }) =
+    at[Map[K, V]](m => JObject(m.toList.map(e => JField(e._1.toString, mtoj(e._2 : V) : JValue))) : JValue)
 
   implicit def atRecord[R <: HList, F, V](implicit folder: MapFolder[R, List[JField], fieldToJSON.type]) =
-    at[R](r => JObject(r.foldMap(Nil: List[JField])(fieldToJSON)(_ ::: _)))
+    at[R](r => JObject(r.foldMap(Nil: List[JField])(fieldToJSON)(_ ::: _)) : JValue)
 }
 
 object fieldToJSON extends Poly1 {
-  implicit def atFieldType[F, V](implicit ftoj: toJSON.Case[V] { type Result <: JValue }, wk: shapeless.Witness.Aux[F]) = at[FieldType[F, V]] {
+  implicit def atFieldType[F, V](implicit ftoj: toJSON.Case[V] { type Result = JValue }, wk: shapeless.Witness.Aux[F]) = at[FieldType[F, V]] {
     f => (Record.keyAsString(f), ftoj(f : V) : JValue) :: Nil
   }
 }
