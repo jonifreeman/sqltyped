d29bfef4d3f6b046dcb555c92711a1f96cb01d99
Joni Freeman <joni.freeman@reaktor.fi>
2014-04-15 17:11:26 +0300

    Release 0.4.0

--- demo/project/build.scala
+++ demo/project/build.scala
@@ -5,13 +5,13 @@ object DemoBuild extends Build {
   lazy val demoSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.example",
     version := "0.4",
-    scalaVersion := "2.10.2",
+    scalaVersion := "2.10.4",
     scalacOptions ++= Seq("-unchecked", "-deprecation"),
     javacOptions ++= Seq("-target", "1.6", "-source", "1.6"),
     crossPaths := false,
     libraryDependencies ++= Seq(
-      "fi.reaktor" %% "sqltyped" % "0.4.0-SNAPSHOT",
-      "fi.reaktor" %% "sqltyped-json4s" % "0.4.0-SNAPSHOT",
+      "fi.reaktor" %% "sqltyped" % "0.4.0",
+      "fi.reaktor" %% "sqltyped-json4s" % "0.4.0",
       "com.typesafe" %% "slick" % "1.0.0-RC1",
       "net.databinder" %% "unfiltered" % "0.6.5",
       "net.databinder" %% "unfiltered-netty" % "0.6.5",
